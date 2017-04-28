a8ef78dd745b5227aac961ae78ed12c25b7967ed
aggerChen <aggerChen@gmail.com>
2017-04-28 14:15:31 +0800

    初始版本

---
+++ Chrome clearCache/background.js
@@ -0,0 +1,45 @@
+﻿
+//监听页面
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    
+    if(request.msg == 'clearCache'){		//如果是清理命令
+		var days = request.days||1;
+		toclean(days,request.data);			//则调用执行清除方法
+    }
+	sendResponse({farewell:true});			//返回信息
+});
+
+//监听快捷键
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == "cleanKey") {				//如果快捷键是指定的按钮
+    toclean(1,{ "appcache": true,			//则调用执行清除方法
+                "cache": true,
+                "cookies": false,
+                "downloads": false,
+                "fileSystems": true,
+                "formData": true,
+                "history": false,
+                "indexedDB": true,
+                "localStorage": true,
+                "serverBoundCertificates": true,
+                "pluginData": true,
+                "passwords": false,
+                "webSQL": true});
+  }
+});
+
+//执行清除方法
+function toclean(days,data){
+	
+	var millisecondsPerWeek = 1000 * 60 * 60 * 24 * days;
+	var ago = (new Date()).getTime() - millisecondsPerWeek;
+	chrome.browsingData.remove({ "since": ago }, data , function () {
+		
+		//弹出框
+		new Notification('chrome chernCache', {
+			icon: 'clean48.png',
+			body: '清理缓存成功!'
+		});
+	});
+	
+}
---
+++ Chrome clearCache/popup.js
@@ -0,0 +1,40 @@
+﻿/*
+* author:aggerChen
+*/
+document.addEventListener('DOMContentLoaded', function () {
+		
+	
+		$("#cleanBtn").on("click",function(){
+				$("#cleanBtn").attr('disabled',true);
+				$("#showMsg").text('缓存清除中...');
+				$("#showMsg").fadeIn();
+				
+				//向扩展程序发送消息，并传递数据
+			   chrome.runtime.sendMessage({
+					msg: 'clearCache',
+					data: getSelectDatas(),			//获取清除选项			
+					days: $("#time").val()			//获取清除多长时间
+				},function(response){
+				   //响应函数
+					$("#cleanBtn").attr('disabled',false);
+					$("#showMsg").text('清除成功!');
+					$('#showMsg').delay(2000).fadeOut(500);
+			   });
+        });
+		
+		//选项监听
+		options.cleanOpt.onchange = function() {	
+			var status = options.cleanOpt.checked;
+			console.log(status);
+		};
+});
+
+//获取选中的清理选项
+function getSelectDatas(){
+	var datas = $("input[name='cleanOpt']");			//获取清除哪些内容
+	var data = {};
+	$.each(datas,function(index,item){					//组装数据
+		if(item.checked) data[item.value] = true;
+	});
+	return data;
+}
